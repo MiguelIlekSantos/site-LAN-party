3365d2472243cc2f6c59d16eb431cd91a20b1879
Miguel Ilek dos Santos <miguelsantosilek@gmail.com>
2024-05-14 11:42:23 +0100

    Update

--- assets/index.js
+++ assets/index.js
@@ -39,84 +39,27 @@ for (let x = 5; x < 50; x++) {
 
 
 
+function checkInputsValidity() {
+    isValid = true; 
 
-
-
-var emailInputs = document.querySelectorAll('input[type="email"]');
-var emails = [];
-
-for (var i = 0; i < emailInputs.length; i++) {
-    (function(i) {
-        emailInputs[i].addEventListener('change', function() {
-            var email = this.value.trim();
-
-            // ----------------VERIFY SCHOOL EMAIL--------------
-
-            // var regex = /^[a-zA-Z0-9._%+-]+[0-9]{7}@a\.esjcff\.pt$/;
-            // if ((!regex.test(email)) && (email != "")) {
-            //     alert("Por favor, insira um email válido da escola.");
-            // } else
-            if (emails.includes(email)) {
-                alert("Você não pode usar o mesmo email para dois participantes. Por favor, corrija os emails.");
-            } else {
-                emails[i] = email;
-            }
-        });
-    })(i);
-}
-
-function checkCheckboxSelection() {
-    var checkboxes = document.querySelectorAll('input[type="checkbox"]');
-    var isChecked = false;
-
-    checkboxes.forEach(function(checkbox) {
-        if (checkbox.checked) {
-            isChecked = true;
+    const selects = document.querySelectorAll('select');
+    selects.forEach(select => {
+        if (select.value === "opção0") {
+            isValid = false; 
         }
     });
 
-    return isChecked;
-}
+    const submitBtn = document.getElementById("submit-btn");
 
-function checkInputsValidity() {
-    var isValid = true;
     
-    for (var i = 0; i < emailInputs.length; i++) {
-        var email = emailInputs[i].value.trim();
-
-        // ----------------VERIFY SCHOOL EMAIL--------------
-        
-        // var regex = /^[a-zA-Z0-9._%+-]+[0-9]{7}@a\.esjcff\.pt$/;
-        // if (!regex.test(email)) {
-        //     emailInputs[i].classList.add('invalid'); 
-        //     isValid = false;
-        // } else {
-        //     emailInputs[i].classList.remove('invalid'); 
-        // }
-        if (email == "") {
-            emailInputs[i].classList.add('invalid'); 
-            isValid = false;
-        } else {
-            emailInputs[i].classList.remove('invalid'); 
-        }
-
-    }
-    if (!checkCheckboxSelection()) {
-        isValid = false      
-    } 
-
-    const submitBtn = document.getElementById("submit-btn")
-
-    if (!isValid) {
-        submitBtn.style.display = "None"
+    if (isValid) {
+        submitBtn.disabled = false;
     } else {
-        submitBtn.style.display = "block"
+        submitBtn.disabled = true;
     }
 }
 
- 
 setInterval(checkInputsValidity, 500);
-
 
 
 
